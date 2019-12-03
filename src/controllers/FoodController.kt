879221c82cab34eea37643c7eb304c141ebb7589
Andrew Kirkham <andrew.nicholas.kirkham@gmail.com>
2019-12-03 15:40:14 -0600

    removing locations experimental api (#2)

--- src/controllers/FoodController.kt
+++ src/controllers/FoodController.kt
@@ -5,8 +5,6 @@ import com.andrew.database.tables.Food
 import com.andrew.database.tables.FoodTable
 import com.andrew.database.tables.Restaurant
 import com.andrew.database.tables.RestaurantTable
-import io.ktor.locations.KtorExperimentalLocationsAPI
-import io.ktor.locations.Location
 import mu.KotlinLogging
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.insert
@@ -14,37 +12,30 @@ import org.jetbrains.exposed.sql.select
 
 private val logger = KotlinLogging.logger {}
 
-@KtorExperimentalLocationsAPI
-@Location("/food")
-class FoodController {
+object FoodController {
 
-    @Location("/{name}")
-    class Search(val name: String) {
-        fun searchByName(): List<Food> {
-            logger.info { "$name was requested" }
-            return loggedTransaction {
-                (FoodTable innerJoin RestaurantTable)
-                    .select {
-                        FoodTable.name.eq(name) and
-                            (FoodTable.restaurantId eq RestaurantTable.id)
-                    }
-                    .map { Food(it[FoodTable.name], Restaurant(it[RestaurantTable.name])) }
-                    .toList()
-            }
+    fun searchByName(name: String): List<Food> {
+        logger.info { "$name was requested" }
+        return loggedTransaction {
+            (FoodTable innerJoin RestaurantTable)
+                .select {
+                    FoodTable.name.eq(name) and
+                        (FoodTable.restaurantId eq RestaurantTable.id)
+                }
+                .map { Food(it[FoodTable.name], Restaurant(it[RestaurantTable.name])) }
+                .toList()
         }
     }
 
-    class Create {
-        /**
-         * Create a new [Food] with the given [String] name and [RestaurantTable] pk
-         */
-        fun createNewFoodAtRestaurant(foodName: String, restaurant: Int): Int {
-            return loggedTransaction {
-                FoodTable.insert {
-                    it[name] = foodName
-                    it[restaurantId] = restaurant
-                } get FoodTable.id
-            }
+    /**
+     * Create a new [Food] with the given [String] name and [RestaurantTable] pk
+     */
+    fun createNewFoodAtRestaurant(foodName: String, restaurant: Int): Int {
+        return loggedTransaction {
+            FoodTable.insert {
+                it[name] = foodName
+                it[restaurantId] = restaurant
+            } get FoodTable.id
         }
     }
 }
--- src/controllers/RestaurantController.kt
+++ src/controllers/RestaurantController.kt
@@ -3,41 +3,32 @@ package com.andrew.controllers
 import com.andrew.database.loggedTransaction
 import com.andrew.database.tables.Restaurant
 import com.andrew.database.tables.RestaurantTable
-import io.ktor.locations.KtorExperimentalLocationsAPI
-import io.ktor.locations.Location
 import mu.KotlinLogging
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 
 private val logger = KotlinLogging.logger {}
 
-@KtorExperimentalLocationsAPI
-@Location("/restaurant")
-class RestaurantController {
+object RestaurantController {
 
-    @Location("/{name}")
-    class Search(val name: String) {
-        fun searchByName(): List<Restaurant> {
-            logger.info { "$name was requested" }
-            return loggedTransaction {
-                RestaurantTable
-                    .select { RestaurantTable.name.eq(name) }
-                    .map { Restaurant(it[RestaurantTable.name]) }
-                    .toList()
-            }
+    fun searchByName(name: String): List<Restaurant> {
+        logger.info { "$name was requested" }
+        return loggedTransaction {
+            RestaurantTable
+                .select { RestaurantTable.name.eq(name) }
+                .map { Restaurant(it[RestaurantTable.name]) }
+                .toList()
         }
     }
 
-    class Create {
-        /**
-         * Create a new [Restaurant] with the given [String] name
-         */
-        fun createNewRestaurant(restaurantName: String): Int {
-            return loggedTransaction {
-                RestaurantTable.insert {
-                    it[name] = restaurantName
-                } get RestaurantTable.id
-            }
+    /**
+     * Create a new [Restaurant] with the given [String] name
+     */
+    fun createNewRestaurant(restaurantName: String): Int {
+        return loggedTransaction {
+            RestaurantTable.insert {
+                it[name] = restaurantName
+            } get RestaurantTable.id
         }
     }
 }
